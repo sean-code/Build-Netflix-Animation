3a9eca06805bd187a11b9370ebee1b19947b7019
sean-code <john.nganga@student.moringaschool.com>
2023-07-21 00:39:49 +0300

    Level 2: Build up on javascript

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
 // index.js
+// const netflixIntro = document.getElementById("netflixintro");
+
+// function hideIntroAndRedirect() {
+//   netflixIntro.style.opacity = "0";
+//   setTimeout(() => {
+//     window.location.href = "https://www.netflix.com"; // Replace with the desired Netflix URL
+//   }, 1000); // Set the time in milliseconds for how long the intro stays visible before redirecting.
+// }
+
+// netflixIntro.addEventListener("click", hideIntroAndRedirect);
+
+
+
+
+
+// level 2
+
+
+// index.js
 const netflixIntro = document.getElementById("netflixintro");
 
 function hideIntroAndRedirect() {
   netflixIntro.style.opacity = "0";
   setTimeout(() => {
-    window.location.href = "https://www.netflix.com"; // Replace with the desired Netflix URL
-  }, 1000); // Set the time in milliseconds for how long the intro stays visible before redirecting.
+    netflixIntro.style.opacity = "1"; // Reset opacity to 1 before starting the animation again
+  }, 4500); // Wait for 4.5 seconds before resetting opacity to 1 (time duration of the animation)
 }
 
-netflixIntro.addEventListener("click", hideIntroAndRedirect);
+// Initial animation
+hideIntroAndRedirect();
+
+// Repeat the animation every 10 seconds
+setInterval(hideIntroAndRedirect, 10000); // 10000 milliseconds = 10 seconds
